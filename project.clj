d2d7e7c9344ca1fb74eb7e8fd03add0726af5daa
mega <mega@patrik-inst.(none)>
2012-04-06 09:15:50 +0200

    added some more function's

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-event "0.1.0-SNAPSHOT"
+(defproject cljs-event "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [jayq "0.1.0-alpha1"]])
--- src/cljs_event/core.cljs
+++ src/cljs_event/core.cljs
@@ -6,7 +6,8 @@ beforeunload click dblclick mousedown mouseup mousemove
 mouseover mouseout mouseenter mouseleave change select
 submit keydown keypress keyup error ")
 
-(def event-handlers (atom {}))
+(def ^{ :doc "holds all event handlers" }
+  event-handlers (atom {}))
 
 (defn attach-handler
   [event-name ref fun]
@@ -14,16 +15,43 @@ submit keydown keypress keyup error ")
     (swap! event-handlers assoc (name event-name) {}))
   (swap! event-handlers update-in [(name event-name)] assoc ref fun))
 
-(defn handler [e]
+(defn trigger
+  "manually trigger some event"
+  [event-name data]
+  (doseq [[key value] (@event-handlers (name event-name))]  
+    (apply value [data])))
+
+(defn handler
+  "the standard callback for all events"
+  [e]
   (let [src (.-srcElement e)
         [event-type event-name] (.split (.data ($ src) "event") ":")]
     (when (= event-type (.-type e))
-      (doseq [[key value] (@event-handlers event-name)]  
-        (apply value [e])))))
+      (trigger (symbol event-name) e))))
 
-(defn setup []
-  (let [$body ($ "body")]
+(defn setup
+  "registers the dom listeners
+    extra-events: adds extra dom events to listen to
+                  default events is under cljs-event.core/events
+  "
+  [extra-events]
+  (let [$body ($ "body")
+        events (if extra-events
+                 (str events " " extra-events)
+                 events)]
     (.delegate $body ".event" events handler)))
 
+
+(defn triggers
+  "creates the data-event string and assoc's it
+   with :data-event
+
+    ex:
+
+     [:input (triggers :click :some-event { :type \"button\" :value \"click me\" })]
+  "
+  [dom-event event-triggered m]
+  (assoc m :data-event
+         (str (name dom-event) ":" (name event-triggered))))
 
 
