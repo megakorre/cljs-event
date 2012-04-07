ecc620ace80e29d787e3cc2fbfe4359a0300e59a
Patrik Kårlin <patrik.karlin@gmail.com>
2012-04-07 09:27:55 +0200

    description update

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject cljs-event "0.1.1-SNAPSHOT"  
   :description "some utility functions to make dom events work
-                nicer with interactive repl development"
+                nicer with interactive repl development
+
+                website: http://www.gitub.com/megakorre/cljs-event"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [jayq "0.1.0-alpha1"]])
